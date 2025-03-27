5db747ae82d2fa57f545c112f1ee6b3f34855f90
Jesse Panganiban <jesse@sageware.io>
2025-03-27 23:33:52 +0800

    Initial Commit

---
+++ Ibon/IbonApp.swift
@@ -0,0 +1,32 @@
+//
+//  IbonApp.swift
+//  Ibon
+//
+//  Created by Jesse Panganiban on 3/27/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct IbonApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
